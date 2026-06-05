d21b223da3d453a5021d4125012d44ae56633f16
Mercy Ma <mercyblitz@gmail.com>
2026-06-05 11:43:52 +0800

    Invoke super.setUp() in test @BeforeEach
    Add a JUnit 5 @BeforeEach setUp method that calls super.setUp() to ensure the base test setup is executed before each test. Also add the corresponding BeforeEach import.

--- microsphere-alibaba-sentinel-plugins/microsphere-alibaba-sentinel-spring-web/src/test/java/io/microsphere/alibaba/sentinel/spring/web/SentinelHandlerMethodInterceptorTest.java
+++ microsphere-alibaba-sentinel-plugins/microsphere-alibaba-sentinel-spring-web/src/test/java/io/microsphere/alibaba/sentinel/spring/web/SentinelHandlerMethodInterceptorTest.java
@@ -20,6 +20,7 @@ package io.microsphere.alibaba.sentinel.spring.web;
 import io.microsphere.spring.test.web.context.request.MockServletWebRequest;
 import io.microsphere.spring.test.webmvc.AbstractWebMvcTest;
 import io.microsphere.spring.webmvc.annotation.EnableWebMvcExtension;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -55,6 +56,11 @@ class SentinelHandlerMethodInterceptorTest extends AbstractWebMvcTest {
     @Test
     void testConstants() {
         assertEquals("sentinelHandlerMethodInterceptor", BEAN_NAME);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
     }
 
     @Test
