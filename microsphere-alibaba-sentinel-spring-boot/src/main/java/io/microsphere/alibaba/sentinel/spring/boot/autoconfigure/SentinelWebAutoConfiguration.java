d5118b49d6fc6c69d58285f891b2f0a34d44a40c
Mercy Ma <mercyblitz@gmail.com>
2026-07-04 14:14:09 +0800

    Register Sentinel web method interceptor
    Refactor SentinelWebAutoConfiguration to focus on web integration instead of Redis interception. The config now applies only to web applications, depends on SentinelHandlerMethodInterceptor classes, and directly declares a missing-bean SentinelHandlerMethodInterceptor. It also updates auto-config ordering to follow Microsphere WebMVC/WebFlux auto-configurations.

--- microsphere-alibaba-sentinel-spring-boot/src/main/java/io/microsphere/alibaba/sentinel/spring/boot/autoconfigure/SentinelWebAutoConfiguration.java
+++ microsphere-alibaba-sentinel-spring-boot/src/main/java/io/microsphere/alibaba/sentinel/spring/boot/autoconfigure/SentinelWebAutoConfiguration.java
@@ -1,54 +1,47 @@
 package io.microsphere.alibaba.sentinel.spring.boot.autoconfigure;
 
-import io.microsphere.alibaba.sentinel.redis.spring.SentinelRedisCommandInterceptor;
 import io.microsphere.alibaba.sentinel.spring.boot.condition.ConditionalOnSentinelAvailable;
-import io.microsphere.redis.spring.annotation.EnableRedisInterceptor;
-import io.microsphere.redis.spring.boot.autoconfigure.condition.ConditionalOnRedisAvailable;
-import io.microsphere.redis.spring.boot.autoconfigure.condition.ConditionalOnRedisInterceptorEnabled;
+import io.microsphere.alibaba.sentinel.spring.web.SentinelHandlerMethodInterceptor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 import static io.microsphere.alibaba.sentinel.redis.SentinelRedisConstants.ENABLED_PROPERTY_NAME;
+import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.ANY;
 
 /**
  * Microsphere Sentinel Spring Boot Auto-Configuration
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @see com.alibaba.cloud.sentinel.custom.SentinelAutoConfiguration
+ * @see org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
+ * @see org.springframework.boot.webmvc.autoconfigure.WebMvcAutoConfiguration
+ * @see io.microsphere.spring.boot.webmvc.autoconfigure.WebMvcAutoConfiguration
+ * @see org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration
+ * @see org.springframework.boot.webflux.autoconfigure.WebFluxAutoConfiguration
+ * @see io.microsphere.spring.boot.webflux.autoconfigure.WebFluxAutoConfiguration
  * @since 1.0.0
  */
+@ConditionalOnWebApplication(type = ANY)
 @ConditionalOnSentinelAvailable
 @ConditionalOnProperty(name = ENABLED_PROPERTY_NAME, matchIfMissing = true)
 @ConditionalOnClass(name = {
-        "io.microsphere.spring.web.method.support.HandlerMethodInterceptor",              // Microsphere Spring Web
-        "io.microsphere.alibaba.sentinel.spring.web.SentinelHandlerMethodInterceptor"     // Microsphere Alibaba Sentinel x Spring Web
+        "io.microsphere.spring.web.method.support.HandlerMethodInterceptor",                        // Microsphere Spring Web
+        "io.microsphere.alibaba.sentinel.spring.web.SentinelHandlerMethodInterceptor"               // Microsphere Alibaba Sentinel x Spring Web
 })
 @AutoConfigureAfter(name = {
         "com.alibaba.cloud.sentinel.custom.SentinelAutoConfiguration",                    // Spring Cloud Alibaba Sentinel
-        "org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration",     // Spring Boot WebMVC
-        "org.springframework.boot.webmvc.autoconfigure.WebMvcAutoConfiguration",          // Spring Boot 4.0+ WebMVC
-        "org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration",   // Spring Boot WebFlux
-        "org.springframework.boot.webflux.autoconfigure.WebFluxAutoConfiguration"         // Spring Boot 4.0+ WebFlux
-})
-@Import(value = {
-        SentinelWebAutoConfiguration.Config.class,
+        "io.microsphere.spring.boot.webmvc.autoconfigure.WebMvcAutoConfiguration",        // Microsphere Spring Boot WebMVC
+        "io.microsphere.spring.boot.webflux.autoconfigure.WebFluxAutoConfiguration"       // Microsphere Spring Boot WebFlux
 })
 public class SentinelWebAutoConfiguration {
 
-    @ConditionalOnRedisAvailable
-    @ConditionalOnRedisInterceptorEnabled
-    @EnableRedisInterceptor
-    static class Config {
-
-        @Bean
-        @ConditionalOnMissingBean
-        public SentinelRedisCommandInterceptor sentinelRedisCommandInterceptor() {
-            return new SentinelRedisCommandInterceptor();
-        }
+    @Bean
+    @ConditionalOnMissingBean
+    public SentinelHandlerMethodInterceptor sentinelHandlerMethodInterceptor() {
+        return new SentinelHandlerMethodInterceptor();
     }
-
 }
