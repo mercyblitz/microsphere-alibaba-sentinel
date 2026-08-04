c78711d14c1ce2c01ffb62087480897d837eccce
Mercy Ma <mercyblitz@gmail.com>
2026-08-04 10:51:51 +0800

    Fix Sentinel scheduler field lookup
    Use the force-access static field lookup when resolving `FlowRuleManager.SCHEDULER` in `SentinelUtils`. This ensures the metrics task executor can still be obtained even if the field is not publicly accessible.

--- microsphere-alibaba-sentinel-commons/src/main/java/io/microsphere/alibaba/sentinel/common/util/SentinelUtils.java
+++ microsphere-alibaba-sentinel-commons/src/main/java/io/microsphere/alibaba/sentinel/common/util/SentinelUtils.java
@@ -156,7 +156,7 @@ public abstract class SentinelUtils {
      * @see FlowRuleManager#SCHEDULER
      */
     public static ScheduledExecutorService findSentinelMetricsTaskExecutor() {
-        return getStaticFieldValue(FlowRuleManager.class, "SCHEDULER");
+        return getStaticFieldValue(true, FlowRuleManager.class, "SCHEDULER");
     }
 
     /**
